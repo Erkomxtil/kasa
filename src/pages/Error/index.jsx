9290fa1216255cf273b9543d43f10ca36c7e04f2
Delastef <delastef@hotmail.comgit config --global user.name delastefegit config --global user.email delastef@hotmail.com>
2023-05-31 16:55:11 +0200

    Mise en place du projet Kasa avec prettier, react router, mise en place de composant de base plus quelques pages

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Error() {
+  return (
+    <div>
+      <h1>404</h1>
+      <div>Oups! La page que vous demandez n'existe pas.</div>
+    </div>
+  )
+}
+
+export default Error
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Home() {
+  return <div>C'est l'accueil !</div>
+}
+
+export default Home
